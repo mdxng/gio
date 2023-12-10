19a552cb90cce97dc2491542daeaa6eb30b8cc2b
mdxng <minhd239@yahoo.de>
2023-12-10 20:04:24 +0100

    added pages

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#home 
+
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+#register page
+
+@app.route('/register/')
+def register():
+    return 'This is the register page'
+
+# login page
+
+@app.route('/login/')
+def login():
+    return 'This is the login page'
+
+# settings page
+
+@app.route('/settings/<int:id>')
+def usersettings(id):
+    return 'This is the settings page'
