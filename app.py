5782821a9ce78281d3a3dd515f9bdbfb1c169b8f
Nekoking14 <s_nguyentie21@stud.hwr-berlin.de>
2024-01-10 20:58:56 +0100

    login and setting ready

--- app.py
+++ app.py
@@ -9,23 +9,36 @@ app.config.from_mapping(
 
 bootstrap = Bootstrap5(app)
 
-# home page
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Handle the form submission (login logic) here
+        return redirect(url_for('home'))
+    else:
+        # Handle GET request (render the login form)
+        return render_template('login.html')
 
 #register page
 
 @app.route('/register/')
 def register():
     return render_template('register.html')
+    
 
-# login page
+   # home page 
+# Change the route for the home page to '/home'
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/login/')
-def login():
-    return render_template('login.html')
+# Set the root URL to redirect to the home page
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+
 
 # settings page
 
@@ -38,3 +51,6 @@ def settings():
 @app.route('/event/')
 def event():
     return render_template('event.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
